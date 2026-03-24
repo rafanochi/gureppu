5e91654ac5ed407731b8db8051fb6e25ca5cdb70
rafa <zawkindev@gmail.com>
2026-03-25 03:05:07 +0500

    feat: basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-#[derive(Debug)]
+use clap::Parser;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+#[derive(Parser, Debug)]
 struct CliArguments {
     pattern: String,
     path: std::path::PathBuf, 
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = CliArguments::parse();
 
-    let args = CliArguments {
-        pattern,
-        path
-    };
+    let file = File::open(&args.path).expect("couldn't read the file");
+    let reader = BufReader::new(file);
+
+    let mut line = String::new();
+
+    for mut line in reader.lines() {
+         if (&mut line).as_mut().expect("Couldn't read the LINE").contains(&args.pattern) {
+            println!("{}", line.unwrap());
+        }
+    }
+
     println!("{:?}", args);
 }
+
+
+
+
