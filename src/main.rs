5951f779af7ba6a4db03202a4db54070104fe86c
rafa <zawkindev@gmail.com>
2026-04-04 03:24:49 +0500

    feat: error handling with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufReader, Error};
 use std::string::String;
-use std::fs::File;
 
 #[derive(Parser, Debug)]
 struct CliArguments {
     pattern: String,
-    path: std::path::PathBuf, 
+    path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = CliArguments::parse();
 
-    let file = File::open(&args.path).expect("couldn't read the file");
+    let file = File::open(&args.path)
+        .with_context(|| format!("couldn't read file: {}", &args.path.display()))?;
+
     let reader = BufReader::new(file);
 
     let result: Vec<String> = reader
         .lines()
         .filter(|x: &Result<String, Error>| x.as_ref().unwrap().contains(&args.pattern))
-        .map(|x| x.unwrap()).collect();
+        .map(|x| x.unwrap())
+        .collect();
 
     println!("{:?}", result);
     println!("{:?}", args);
+
+    Ok(())
 }
